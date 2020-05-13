0843a2789e84afc734bc781531d5042943956a1e
BrockBatBlue <59485751+BrockBatBlue@users.noreply.github.com>
2020-05-13 17:58:27 -0500

    Created the AJAX GET methods

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=d8c80ace4b34f001fb4f2334a529a592";
-$.ajax({
-    url: queryURL,
-    method: "GET"
+var city
+
+var getWeather = function (city){
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d8c80ace4b34f001fb4f2334a529a592";
+    $.ajax({
+        url: weatherURL,
+        method: "GET"
     }).then(function(response) {
-    createRow(response);
+        console.log(response);
     });
+}
+
+var getForecast = function(city){
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&appid=d8c80ace4b34f001fb4f2334a529a592&cnt=5";
+    $.ajax({
+        url: forecastURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    });
+}
